76380fb3923a3bf49e43c3b6e6345f41bed879c4
froemming13 <mikaylafroemming@gmail.com>
2026-03-02 14:01:09 -0600

    maybe ill get it this time

--- read_movies.py
+++ read_movies.py
@@ -1,6 +1,7 @@
 # read_movies.py
 # Reads all items from the DynamoDB Movies table and prints them.
 # Part of Lab 09 — feature/read-dynamo branch
+# mb this has to be here
 
 import boto3
 from boto3.dynamodb.conditions import Key
@@ -54,23 +55,23 @@ def print_all_movies():
 
 def get_movie_by_title():
     table = get_table()
-    user_title = input("Enter a movie title: ")
+    user_title = input("Enter a Movie Title: ")
 
-    response = table.scan("FilterExpression":"title")
-
-    items = response.get("Items", [])
+    response = table.scan(FilterExpression=Attr("Title").eq(user_title))
+    items = response.get("Items",[])
 
     if items:
-        print("Here is your movie: ")
-        for item in items:
-            print(item)
+        print("Movie Found: ")
+        for movie in items:
+            print_movie(movie)
     
     else:
-        print("No Movie Found")
+        print("Move Not Found")
 
 def main():
     print("===== Reading from DynamoDB =====\n")
     print_all_movies()
+    get_movie_by_title()
 
 
 if __name__ == "__main__":
